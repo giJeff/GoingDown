e3ab898ed27412bbe3f8a933a28cd2541cebac0f
giJeff <jgraves4@ggc.edu>
2017-04-12 16:50:34 -0400

    adding files for edit in class
    some stuff goes here

---
+++ GoingDown-develop/src/controller/Item.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Item {
+	private String itemName;
+	private double itemWeight;
+
+	 
+	public String getItemName() {
+		return itemName;
+	}
+	
+	public void setItemName(String itemName) {
+			this.itemName = itemName;
+		}
+	public double getItemWeight() {
+		return itemWeight;
+	}
+
+	public void setItemWeight(double itemWeight) {
+		this.itemWeight = itemWeight;
+	}
+}
