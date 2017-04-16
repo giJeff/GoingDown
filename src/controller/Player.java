6da7d200d97bd76e0d10dd828995e66a8bd4eca4
Charneaka Lewis <clewis20@ggc.edu>
2017-04-16 18:30:12 -0400

    Created simple Player class

---
+++ src/controller/Player.java
@@ -0,0 +1,54 @@
+package controller;
+
+/** Class : Player.java
+ * @author: Charneaka Lewis
+ * @version: 1.0
+ * Course: ITEC 3860
+ * Written: April 16, 2017
+ *
+ * This class is the Player class handling business logic for the Player 
+ */
+
+public class Player
+{ 
+	private String playerName;
+	private int score;
+	private int health; 
+
+	public Player(String playerName, int score, int health) {
+		super();
+		this.playerName = playerName;
+		this.score = score;
+		this.health = health;
+	}
+
+	public String getPlayerName() 
+	{
+		return playerName;
+	}
+	
+	public void setPlayerName(String playerName) 
+	{
+		this.playerName = playerName;
+	}
+	
+	public int getScore() 
+	{
+		return score;
+	}
+	
+	public void setScore(int score) 
+	{
+		this.score = score;
+	}
+	
+	public int getHealth() 
+	{
+		return health;
+	}
+	
+	public void setHealth(int health) 
+	{
+		this.health = health;
+	}
+}
